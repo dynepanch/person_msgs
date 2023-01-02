889e710240eecbd365d6e038e3a722cf9dca491d
KenInaba <jiandaoye293@gmail.com>
2023-01-02 23:36:23 +0900

    kfs

---
+++ test.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+#SPDX-FileCopyrightText: 2022 Ken Inaba rightman20020920@yahoo.co,jp
+#SPDX-Licence-Identifire: BSD-3-Clause
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
+
+ros2 interface show person_msgs/msg/Person
